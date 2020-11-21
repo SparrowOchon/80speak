110b58f75175e75161ba7ef23a552416144586f0
Andre Bhaseen <34148840+abhaseen@users.noreply.github.com>
2020-11-21 03:23:22 -0500

    Made initial script, wip build v0.1

--- dectalk/process.py
+++ dectalk/process.py
@@ -12,39 +12,39 @@ RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 
-# creating and opening the stream
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+# # creating and opening the stream
+# stream = p.open(format=FORMAT,
+#                 channels=CHANNELS,
+#                 rate=RATE,
+#                 input=True,
+#                 frames_per_buffer=CHUNK)
 
-print("* recording")
+# print("* recording")
 
-frames = []
+# frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#     data = stream.read(CHUNK)
+#     frames.append(data)
 
-print("* done recording")
+# print("* done recording")
 
-# Stopping the stream
-stream.stop_stream()
-stream.close()
-p.terminate()
+# # Stopping the stream
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
 
-print("Writing File...")
+# print("Writing File...")
 
-# writing the file
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+# # writing the file
+# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+# wf.setnchannels(CHANNELS)
+# wf.setsampwidth(p.get_sample_size(FORMAT))
+# wf.setframerate(RATE)
+# wf.writeframes(b''.join(frames))
+# wf.close()
 
 print("Using recognizer...")
 
